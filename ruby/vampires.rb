8fda1047a2ed2d5fb1a520d8c8c830f5bceb78ce
Sheena Vasani <sheenavasani1@gmail.com>
2016-12-08 15:40:26 -0800

    Add if less than 100 and yes to garlic bread or health insurance, then probably not a vampire

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,7 +1,7 @@
 puts "what is your name?"
 name = gets.chomp
 
-puts "how old are you? what you were you born?"
+puts "how old are you? what year were you born?"
 age_year = gets.chomp
 
 puts "should we order some garlic bread for you?"
@@ -9,3 +9,7 @@ garlic_bread = gets.chomp
 
 puts "would you like to enroll in the company's health insurance?"
 health_insurance = gets.chomp
+
+if age_year > "100" && garlic_bread == "yes" || health_insurance == "yes"
+	puts "Probably not a vampire"
+end
