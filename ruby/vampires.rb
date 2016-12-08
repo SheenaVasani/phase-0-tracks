40ea76d88ddd5f9f618b0fd266a5143a271705f7
Sheena Vasani <sheenavasani1@gmail.com>
2016-12-08 10:32:06 -0800

    Ask employee basic questions to gather data

---
+++ ruby/vampires.rb
@@ -0,0 +1,11 @@
+puts "what is your name?"
+name = gets.chomp
+
+puts "how old are you? what you were you born?"
+age_year = gets.chomp
+
+puts "should we order some garlic bread for you?"
+garlic_bread = gets.chomp
+
+puts "would you like to enroll in the company's health insurance?"
+health_insurance = gets.chomp
