b018643a6318170cf44339fe2c5823d54763be43
Sheena Vasani <sheenavasani1@gmail.com>
2016-12-09 13:40:17 -0800

    Finished up adding all conditions necessary to determine if vampire for release 2

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,8 +1,8 @@
 puts "what is your name?"
-name = gets.chomp
+name = gets.chomp 
 
 puts "how old are you? what year were you born?"
-age_year = gets.chomp
+age_year = gets.chomp.to_i
 
 puts "should we order some garlic bread for you?"
 garlic_bread = gets.chomp
@@ -10,9 +10,21 @@ garlic_bread = gets.chomp
 puts "would you like to enroll in the company's health insurance?"
 health_insurance = gets.chomp
 
-if age_year < "100" && garlic_bread == "yes" || health_insurance == "yes"
+if age_year < 100 && garlic_bread == "yes" || health_insurance == "yes"
 	puts "Probably not a vampire"
-end
-else age_year > "100" && garlic_bread == "no" || health_insurance == "no"
+elsif age_year > 100 && garlic_bread == "no" 
   puts "Probably a vampire"
-end
+elsif age_year > 100 && health_insurance == "no"
+  puts "Probably a vampire"
+elsif age_year > 100 && garlic_bread == "no" && health_insurance == "no"
+  puts "Almost certainly a vampire"
+else 
+	age_year > 100 && garlic_bread == "yes" && health_insurance == "yes"
+end 
+
+if name == "Drake Cula" || name == "Tu Fang"
+  puts "Definitely a vampire"
+else
+	puts "Results inconclusive"
+end 
+
