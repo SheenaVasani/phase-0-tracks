306c23994da590c45ef2ffa198e4df2b7a1dcfa5
Sheena Vasani <sheenavasani1@gmail.com>
2016-12-18 15:26:42 -0800

    Add loop to method so user can keep adding fake names repeatedly

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,18 +1,31 @@
+until name = false 
+
 puts "What is your first name?"
-first_name = gets.chomp.downcase
+first_name = gets.chomp
+name = true 
+
+if first_name == "quit" 
+      name = false 
+      break 
+end 
 
 puts "What is your last name?"
-last_name = gets.chomp.downcase
+last_name = gets.chomp
+name = true 
 
+if last_name == "quit" 
+      name = false 
+      break 
+end 
 
 def fake_name (first_name, last_name)
   "#{last_name} #{first_name}"
 end 
 
 def name_converter(first_name, last_name)
-	#1. swap the order names
+  #1. swap the order names
   swapped_names = fake_name(first_name, last_name)
-	#2 break swappedNames into array
+  #2 break swappedNames into array
   character_array = swapped_names.split('')
   #3. iterate through array
   vowel = ["a","e","i","o","u"]
@@ -31,7 +44,6 @@ def name_converter(first_name, last_name)
         new_index = current_index + 1
         #3. go to vowel array with new_index and retrieve that letter
         # next_letter = 'i'
-
         # add next_letter to result
         result += vowel[new_index]
 
@@ -52,164 +64,11 @@ def name_converter(first_name, last_name)
   end
   #4. if the character is a vowel do a vowel swap
   #5. elsif the character is a consonant do a consonant swap
-	
+  
   result
 end
 
 p name_converter(first_name, last_name)
 
-# [b, o, b, , s, u, e]
-
-# b
-# o
-# b
-
-# s
-# u
-# e
-
-
-
-#Create method to move up one vowel or one consonant 
-# def next_vowel (name)
-# #Convert name into an array of letters
-#   "name".split ('')
-#   name.class
-
-# #Create two arrays for vowels and consonants
-#   vowel = ["a","e","i","o","u"]
-#   consonant = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x","y","z"]
-# #Iterate through array of letters in name
-# #If a letter in name matches a vowel, move forward one vowel
-# #If a letter in name matches a consonant, move forward one consonant
-# name.each do |name|
-#   puts name 
-#   if name[index]! == vowel[index] then 
-#     vowel[index].next
-#   else
-#     if name[index]! == consonant[index] then
-#       consonant[index].next
-# end
-
-
-#Create method to swap letters of name
-
-
-
-
-
-# -----
-
-# puts "What is your first name?"
-# first_name = gets.chomp
-
-# puts "What is your last name?"
-# last_name = gets.chomp
-
-# def name 
-  
-#   name = "first_name" + "last_name"
-
-#   def fake_name (first_name, last_name)
-#     fake_name = "first_name" + "last_name"
-#     last_name.replace(first_name)
-#   end 
-
-# #Pass this method into second method next_vowel
-# #Create next_vowel method to move up one vowel or one consonant 
-#   def next_vowel (name)
-#     name = fake_name (name)
-# #Convert name into an array of letters
-#     "name".split ('')
-#     name.class
-
-# #Create two arrays for vowels and consonants
-#     vowel = ["a","e","i","o","u"]
-#     consonant = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x","y","z"]
-# #Iterate through array of letters in name
-# #If a letter in name matches a vowel, move forward one vowel
-# #If a letter in name matches a consonant, move forward one consonant
-#       name.each do |name|
-#         puts name 
-#     if name[index]! == vowel[index] then 
-#       vowel[index].next
-#     else
-#       if name[index]! == consonant[index] then
-#       consonant[index].next
-#       end 
-#    end 
-
-
-#    return name
-# end 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# ------------
-# #i want this method to take the person's name and swap their places/reverse their order
-
-# def fake_name (first_name, last_name)
-#   x = "last_name"
-#   x[4]= " first_name"
-# end 
-
-# def fake_name (string_1, string_2)
-#   string_1 + string_2. sub (string_2, string_1)
-
-#   def fake (first, last)
-#   puts "last" + "first"
-# end 
-
-#   puts fake ("te","er")
-
-# puts "The cat and the hat".sub("hat", "rat")   #=> The cat and the rat
-# puts "Another brick in the wall".sub("brick in the", "")   #=>   "Another wall"
-      
-# The first argument is the character(s) to replace. The second argument is what to replace it with.
-
-# The sub method replaces only the first occurrence of the string to match. If you want to replace all occurrences of a given string, use gsub, which is short for global substitution:
-
-# #i want  the program to go through each character of my reversed name and compare it to each
-# #character in my vowel array. if they are the same, or equal to, i want to take the reversed name character 
-# #that is equal to the character in the vowel array, and i want to push it forward to the next vowel array item.
-
-# #i want to do the same for consonants.
-
-  
-# when name[index] = vowels[index]
-#   puts vowels.next
-
-#   i want you to iterate through the string array, select the vowels, then push those forward
-#   (isn't there a select array thing in documentation?)
-# ----
-
-# "gets.chomp" index ("a")
-
-#   -pseudocode
-#   -alias method look up
-#   -look at how to put methods inside methods
-#   -office hours evening(have this organized for it)
-#   -slack keep talking about this with ta
-#   -codeacademy 
-
+end 
 
