52cfcaf0207b1ec94598780bec3a0c1df1a30bb3
Sheena Vasani <sheenavasani1@gmail.com>
2016-12-21 18:37:05 -0800

    Finished making methods to add, remove items, update quantity and print list, also include driver code

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -10,8 +10,34 @@ def create_a_list(items)
     list 
 end 
 
+def add_item (list, item, quantity)
+    list[item] = quantity
+    list
+end 
+
+def remove_item(list, item)
+    list.delete(item)
+    list
+end 
+
+def update_quantity(list, item, quantity)
+    list[item] = quantity
+    list 
+end 
+
+def print_list(list)
+    puts "Here is your grocery list:"
+    list.each do |item, quantity| 
+        puts "#{item}: #{quantity}"
+    end 
+end 
 
 
+groceries = create_a_list ("lemonade tomatoes onions ice_cream")
+p add_item(groceries, "kitten", 1)
+p remove_item(groceries, "lemonade")
+p update_quantity(groceries, "ice_cream", 4)
+print_list(groceries)
 
 
 # Method to create a list
@@ -40,4 +66,19 @@ end
 # Method to print a list and make it look pretty
 # input: take in the hash 
 # steps: print each item/quantity pair as a sentence
-# output: sentence 
+# output: hash 
+
+#RELEASE 4:
+#I learned how to write more specific pseudocode that helps me identify input and output. I realized
+#I've been writing bad pseudocode for awhile now, so this helped.
+
+#Arrays were good for splitting the strings into characters, while hashes were good for descriptive purposes.
+
+#A method returns whatever value is written on the last line 
+
+#You can pass integers, strings and even methods into arguments 
+
+#You can pass information between methods by including a method into the other method's argument and
+#being sure to return the value
+
+#I think the idea of returns and passing methods became clearer to me, but I still need to understand scope better
