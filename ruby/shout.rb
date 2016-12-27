b434ae953e2009709015248f38860db8370ba399
Sheena Vasani <sheenavasani1@gmail.com>
2016-12-26 17:03:51 -0800

    add classes man and baby, include module

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,16 +1,36 @@
 module Shout
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
+  def how(adverb)
+    puts "I AM #{adverb} HUNGRY!"
   end
+end
 
-  def self.yelling_happily(happy_words)
-    happy_words + "!!!" + " :)"
-    end
-
+class Baby
+    include Shout
 end 
 
-require_relative 'shout'
-Shout.yell_angrily("meow")
+class Man
+    include Shout
+end   
 
-require_relative 'shout'
-Shout.yelling_happily("meow")
+baby = Baby.new
+baby.how("SO")
+
+man = Man.new
+man.how("NOT")
+
+# module Shout
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+
+#   def self.yelling_happily(happy_words)
+#     happy_words + "!!!" + " :)"
+#     end
+
+# end 
+
+# require_relative 'shout'
+# Shout.yell_angrily("meow")
+
+# require_relative 'shout'
+# Shout.yelling_happily("meow")
