b816832136da8929da661140dd72ed52c96aa757
Sheena Vasani <sheenavasani1@gmail.com>
2017-01-15 16:06:08 -0800

    add refactor

--- databases/8.5_assignment/life_balance.rb
+++ databases/8.5_assignment/life_balance.rb
@@ -29,7 +29,6 @@ def create_table(db, name, work, study, relationships, sleep, leisure, exercise,
     db.execute("INSERT INTO life_balance (name, work, study, relationships, sleep, leisure, exercise, happiness) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", [name, work, study, relationships, sleep, leisure, exercise, happiness])
 end
 
-
 puts "Today we are going to track how many hours you spent on each area of your life."
 puts "This information will help you assess how balanced your life is as of right now, and where to improve."
 puts "To start, please provide your first name."
@@ -50,6 +49,6 @@ puts "Overall, how happy do you feel with how you spent your time today?"
 
 balance = db.execute("SELECT * FROM life_balance")
 balance.each do |area|
-    puts "#{life_balance['name']} today you spent #{life_balance['work']} hours working, while you studied for #{life_balance['study']} hours. Thankfully, your entire day wasn't all about work! You also spent #{life_balance['relationships']}
-        hours with your loved ones and #{life_balance['leisure']} on other enjoyable activities. You also spent some taking care of your physical health; you exercised for #{life_balance['exercise']} hours. Finally, you got some rest, sleeping for #{life_balance['sleep']} hours."
+    puts "#{area['name']} today you spent #{area['work']} hours working, while you studied for #{area['study']} hours. Thankfully, your entire day wasn't all about work! You also spent #{area['relationships']}
+        hours with your loved ones and #{area['leisure']} on other enjoyable activities. You also spent some taking care of your physical health; you exercised for #{area['exercise']} hours. Finally, you got some rest, sleeping for #{area['sleep']} hours."
     end
